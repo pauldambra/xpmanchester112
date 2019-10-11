4b54e1fccdc8447e5093c32b076aa199584a2c33
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2019-10-11 21:26:21 +0100

    not using typescript

---
+++ test/wat.spec.js
@@ -0,0 +1,28 @@
+const expect = require('chai').expect
+
+describe ('bowling', function() {
+  it('scores two fives', function() {
+    const score = bowl('55');
+    expect(score).to.equal(10);
+  })
+
+  it('scores six and a three', function() {
+    const score = bowl('63');
+    expect(score).to.equal(9);
+  })
+
+  it('scores over two frames', function () {
+    const score = bowl('63 63');
+    expect(score).to.equal(18);
+  })
+
+})
+
+const parse = (x) => x.split('').filter(s => s != ' ')
+
+function bowl(x)
+{
+  return parse(x)
+    .map(s => parseInt(s, 10))
+    .reduce((l,r) => l + r, 0)
+}
